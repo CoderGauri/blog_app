ba8d479cddc96ab495b63f5e12db0142035b0e77
Gauri <gauriM304@gmail.com>
2024-04-09 12:48:27 +0530

    Basic form added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      Hello World
-      </div>
+      <h1>Create a New Blog Post</h1>
+      <form>
+        <div className="form-group">
+          <label htmlFor="postName">Post Name:</label>
+          <input type="text" id="postName" required />
+        </div>
+        <div className="form-group">
+          <label htmlFor="description">Description:</label>
+          <textarea id="description" required />
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
   );
 }
 
